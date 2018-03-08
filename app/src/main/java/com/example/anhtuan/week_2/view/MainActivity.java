cfad4308596a48112e901b2620e1b9bdf5e02d1c
Jason <jason@vinova.sg>
2018-03-08 16:58:43 +0700

    finished

--- app/src/main/java/com/example/anhtuan/week_2/view/MainActivity.java
+++ app/src/main/java/com/example/anhtuan/week_2/view/MainActivity.java
@@ -8,8 +8,8 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -101,9 +101,20 @@ public class MainActivity extends AppCompatActivity implements IArticle.IView {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_icon, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu_icon, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.icon_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -115,7 +126,7 @@ public class MainActivity extends AppCompatActivity implements IArticle.IView {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.icon_search:
-                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.icon_filter:
                 Intent intent = new Intent(MainActivity.this, FilterActivity.class);
@@ -137,4 +148,5 @@ public class MainActivity extends AppCompatActivity implements IArticle.IView {
         swipeRefreshLayout.setRefreshing(false);
         makeText(this, "Show Fail", Toast.LENGTH_SHORT).show();
     }
+
 }
